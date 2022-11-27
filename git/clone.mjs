8141e34812c984eef4e7e0419bb5a8cabba3d82c
mesoic <mesoic@protonmail.com>
2022-11-26 22:40:01 -0600

    update clone

--- git/clone.mjs
+++ git/clone.mjs
@@ -6,7 +6,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 $.verbose = false
+let origin = process.argv[3]
 let passwd = fs.readFileSync("/usr/local/sbin/git/.passwd", "utf8")
-let res = await $ `expect -f /usr/local/sbin/git/tcl/clone.tcl ${passwd}`
+let res = await $ `expect -f /usr/local/sbin/git/tcl/clone.tcl ${passwd} ${origin}`
 $.verbose = true
 console.log(res.stdout)
--- git/tcl/clone.tcl
+++ git/tcl/clone.tcl
@@ -1,6 +1,7 @@
 #! /usr/bin/expect
 set timeout -1
 set GIT_PASSWD [lindex $argv 0]
+set GIT_ORIGIN [lindex $argv 1]
 
 spawn git clone
 expect {
